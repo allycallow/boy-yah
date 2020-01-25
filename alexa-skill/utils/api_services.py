521b8e03451e9baa07a3c7145c509b7878c80335
Alex Callow <allycallow@mobile-5.com>
2020-01-25 16:15:44 +0000

    Added post request

--- alexa-skill/utils/api_services.py
+++ alexa-skill/utils/api_services.py
@@ -9,7 +9,7 @@ logger.setLevel(logging.INFO)
 API_ENDPOINT = "https://rest.ehrscape.com/rest/v1/composition?ehrId=5f4401c5-071a-4b4e-8426-6f2ccb32fb04&templateId=NHSHD23%20BoYaA&committerName=Alexa&format=FLAT"
 TOKEN = "am9obi5tZXJlZGl0aEB3YWxlcy5uaHMudWs6ZWhyNGpvaG4ubWVyZWRpdGg="
 
-COMPOSER_NAME = "Yann McN"
+COMPOSER_NAME = "Alexa"
 LANGUAGE = "en"
 TERRITORY = "GB"
 
@@ -27,11 +27,13 @@ class ApiServices:
             "nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
         }
 
-        print(data)
-
         headers = {'content-type': 'application/json', "Authorization": f"Basic {TOKEN}"}
         r = requests.post(url=API_ENDPOINT, data=json.dumps(data), headers=headers)
+
         print(r.status_code)
-        print(r)
-        print(r.text)
         print(r.json())
+
+        if r.status_code == 200:
+            logger.info("successfully stored record")
+            return r.json()
+        raise Exception("something went wrong")
