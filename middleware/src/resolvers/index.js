2890ecd344ed8a3120320dcfdc8061a039a1162e
Alex Callow <allycallow@mobile-5.com>
2020-01-25 17:13:49 +0000

    Created mutation

--- middleware/src/resolvers/index.js
+++ middleware/src/resolvers/index.js
@@ -2,10 +2,20 @@ const getTest = async () => ({
   ok: true
 });
 
+
+const storeBloodPressure = async () => ({
+  ok: false
+})
+
+
 const resolvers = {
   Query: {
     test: getTest
   }
+  Mutation: {
+    store_blood_pressure: storeBloodPressure
+  }
+
 };
 
 export default resolvers;
