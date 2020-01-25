5a00e52802e8d181a0cbe22fe95fdabb0f245365
Alex Callow <allycallow@mobile-5.com>
2020-01-25 17:37:45 +0000

    Fixed token issue:

--- middleware/src/resolvers/index.js
+++ middleware/src/resolvers/index.js
@@ -1,57 +1,45 @@
+import fetch from "node-fetch";
+
 const getTest = async () => ({
   ok: true
 });
 
-
-const storeBloodPressure = async (root, args, context, info) => {
-  const {  BACKEND_ENDPOINT, TOKEN } = proces.env;
+const storeBloodPressure = async (root, { value }, context, info) => {
+  const { API_ENDPOINT } = process.env;
 
   const data = {
     "ctx/composer_name": "Alex Callow",
     "ctx/language": "en",
     "ctx/territory": "GB",
-    "nhshd23_boyaa/blood_pressure/systolic|magnitude": 2,
+    "nhshd23_boyaa/blood_pressure/systolic|magnitude": value,
     "nhshd23_boyaa/blood_pressure/systolic|unit": "mm[Hg]",
     "nhshd23_boyaa/blood_pressure/diastolic|magnitude": 80,
     "nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
-  }
+  };
 
-  const res await fetch(BACKEND_ENDPOINT, {
-    method: 'POST',
-    body: JSON.stringify(data) },
+  const res = await fetch(API_ENDPOINT, {
+    method: "POST",
+    body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json',
-      "Authorization": `Basic ${TOKEN}`
+      "Content-Type": "application/json",
+      Authorization: `Basic am9obi5tZXJlZGl0aEB3YWxlcy5uaHMudWs6ZWhyNGpvaG4ubWVyZWRpdGg=`
     }
   });
 
-  console.log(res.json());
+  console.log(await res.json());
 
   return {
     ok: true
-  }
-}
-
+  };
+};
 
 const resolvers = {
   Query: {
     test: getTest
-  }
+  },
   Mutation: {
     store_blood_pressure: storeBloodPressure
   }
-
 };
 
 export default resolvers;
-
-
-{
-	"ctx/composer_name": "Yann McN",
-	"ctx/language": "en",
-	"ctx/territory": "GB",
-	"nhshd23_boyaa/blood_pressure/systolic|magnitude": 2,
-	"nhshd23_boyaa/blood_pressure/systolic|unit": "mm[Hg]",
-	"nhshd23_boyaa/blood_pressure/diastolic|magnitude": 80,
-	"nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
-}
