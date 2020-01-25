0b1436e179d99d86d55389344601a2b87b6aa56a
Alex Callow <allycallow@mobile-5.com>
2020-01-25 19:26:38 +0000

    Added simple error handling

--- middleware/src/resolvers/index.js
+++ middleware/src/resolvers/index.js
@@ -1,19 +1,24 @@
+/* eslint-disable no-console */
 import fetch from "node-fetch";
+import { ApolloError } from "apollo-server";
 
 const getTest = async () => ({
   ok: true
 });
 
-const storeBloodPressure = async (root, { value }) => {
+const storeBloodPressure = async (
+  root,
+  { systolic_number, diastolic_number }
+) => {
   const { API_ENDPOINT, TOKEN } = process.env;
 
   const data = {
     "ctx/composer_name": "Alex Callow",
     "ctx/language": "en",
     "ctx/territory": "GB",
-    "nhshd23_boyaa/blood_pressure/systolic|magnitude": value,
+    "nhshd23_boyaa/blood_pressure/systolic|magnitude": systolic_number,
     "nhshd23_boyaa/blood_pressure/systolic|unit": "mm[Hg]",
-    "nhshd23_boyaa/blood_pressure/diastolic|magnitude": 80,
+    "nhshd23_boyaa/blood_pressure/diastolic|magnitude": diastolic_number,
     "nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
   };
 
@@ -26,11 +31,15 @@ const storeBloodPressure = async (root, { value }) => {
     }
   });
 
+  // * PRINT RESULT *
+  console.log(res.ok);
   console.log(await res.json());
 
-  return {
-    ok: true
-  };
+  if (res.ok) {
+    return await res.json();
+  }
+
+  throw new ApolloError("Could not store blood pressure results", 500);
 };
 
 const resolvers = {
