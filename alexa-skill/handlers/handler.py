509ae45b5fec4a522195002079a6973a3f2f00a3
Alex Callow <allycallow@mobile-5.com>
2020-01-25 14:32:41 +0000

    Added alexa skill

--- alexa-skill/handlers/handler.py
+++ alexa-skill/handlers/handler.py
@@ -13,3 +13,22 @@ def help_intent_handler(handler_input):
     logger.info("In HelpIntentHandler")
     handler_input.response_builder.speak("Boom Boom").ask("Please go again")
     return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("BloodPressure"))
+def blood_pressure_intent_handler(handler_input):
+    """Handler for Lucky Number sIntent."""
+    logger.info(f"BloodPressure")
+    response_builder = handler_input.response_builder
+
+    print(handler_input.request_envelope.request)
+    number = handler_input.request_envelope.request.intent.slots[
+        "number"
+    ].value
+
+    speech_text = f"working {number}"
+
+    return response_builder.speak(speech_text).set_should_end_session(False).response
+
+
+handler = sb.lambda_handler()
