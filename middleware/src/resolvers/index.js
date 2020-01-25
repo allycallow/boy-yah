ca8ddb8b047b64e2de8317657c5da9ef0856b342
Alex Callow <allycallow@mobile-5.com>
2020-01-25 17:19:32 +0000

    Added fetch request

--- middleware/src/resolvers/index.js
+++ middleware/src/resolvers/index.js
@@ -3,9 +3,34 @@ const getTest = async () => ({
 });
 
 
-const storeBloodPressure = async () => ({
-  ok: false
-})
+const storeBloodPressure = async (root, args, context, info) => {
+  const {  BACKEND_ENDPOINT, TOKEN } = proces.env;
+
+  const data = {
+    "ctx/composer_name": "Alex Callow",
+    "ctx/language": "en",
+    "ctx/territory": "GB",
+    "nhshd23_boyaa/blood_pressure/systolic|magnitude": 2,
+    "nhshd23_boyaa/blood_pressure/systolic|unit": "mm[Hg]",
+    "nhshd23_boyaa/blood_pressure/diastolic|magnitude": 80,
+    "nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
+  }
+
+  const res await fetch(BACKEND_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify(data) },
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": `Basic ${TOKEN}`
+    }
+  });
+
+  console.log(res.json());
+
+  return {
+    ok: true
+  }
+}
 
 
 const resolvers = {
@@ -19,3 +44,14 @@ const resolvers = {
 };
 
 export default resolvers;
+
+
+{
+	"ctx/composer_name": "Yann McN",
+	"ctx/language": "en",
+	"ctx/territory": "GB",
+	"nhshd23_boyaa/blood_pressure/systolic|magnitude": 2,
+	"nhshd23_boyaa/blood_pressure/systolic|unit": "mm[Hg]",
+	"nhshd23_boyaa/blood_pressure/diastolic|magnitude": 80,
+	"nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
+}
