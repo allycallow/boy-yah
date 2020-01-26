c240ac3275c487dad4d1295c9b110f67888b4de6
Alex Callow <allycallow@mobile-5.com>
2020-01-26 12:29:00 +0000

    Headers

--- alexa-skill/utils/api_services.py
+++ alexa-skill/utils/api_services.py
@@ -15,6 +15,9 @@ TERRITORY = "GB"
 
 
 class ApiServices:
+    def __init__(self):
+        self.headers = {'content-type': 'application/json', "Authorization": f"Basic {TOKEN}"}
+
     def store_blood_pressure(self, systolic_number, diastolic_number):
         logger.info("store_blood_pressure fired")
         data = {
@@ -27,8 +30,7 @@ class ApiServices:
             "nhshd23_boyaa/blood_pressure/diastolic|unit": "mm[Hg]"
         }
 
-        headers = {'content-type': 'application/json', "Authorization": f"Basic {TOKEN}"}
-        r = requests.post(url="{API_ENDPOINT}/composition?ehrId=5f4401c5-071a-4b4e-8426-6f2ccb32fb04&templateId=NHSHD23%20BoYaA&committerName=Alexa&format=FLAT", data=json.dumps(data), headers=headers)
+        r = requests.post(url=f"{API_ENDPOINT}/composition?ehrId=5f4401c5-071a-4b4e-8426-6f2ccb32fb04&templateId=NHSHD23%20BoYaA&committerName=Alexa&format=FLAT", data=json.dumps(data), headers=self.headers)
 
         print(r.status_code)
         print(r.json())
@@ -48,8 +50,7 @@ class ApiServices:
             "nhshd23_boyaa/peak_flow_rate/result_details/pulmonary_flow_rate_result|unit": "l/min"
         }
 
-        headers = {'content-type': 'application/json', "Authorization": f"Basic {TOKEN}"}
-        r = requests.post(url=f"{API_ENDPOINT}/composition?ehrId=5f4401c5-071a-4b4e-8426-6f2ccb32fb04&templateId=NHSHD23%20BoYaA&committerName=Alexa&format=FLAT", data=json.dumps(data), headers=headers)
+        r = requests.post(url=f"{API_ENDPOINT}/composition?ehrId=5f4401c5-071a-4b4e-8426-6f2ccb32fb04&templateId=NHSHD23%20BoYaA&committerName=Alexa&format=FLAT", data=json.dumps(data), headers=self.headers)
 
         print(r.status_code)
         print(r.json())
@@ -65,13 +66,14 @@ class ApiServices:
             "aql": "select a_a/data[at0001|Event Series|]/events[at0002|Any event|]/data[at0003]/items[at0127|Result details|]/items[at0057|Pulmonary flow rate result|]/items[at0058|Actual result|]/value/magnitude as Result_Value, a_a/data[at0001|Event Series|]/origin/value as Time, a_a/data[at0001|Event Series|]/events[at0002|Any event|]/state[at0031]/items[at0098|Confounding factors|]/value/value as Setting, a_a/data[at0001|Event Series|]/events[at0002|Any event|]/state[at0031]/items[at0049|Bronchodilation|]/items[at0051|Agent|]/value/value as Agent, a_a/data[at0001|Event Series|]/events[at0002|Any event|]/state[at0031]/items[at0049|Bronchodilation|]/items[at0091|Timing|]/value/value as Challenge_State from EHR e contains COMPOSITION a contains OBSERVATION a_a[openEHR-EHR-OBSERVATION.pulmonary_function.v0] order by a_a/data[at0001|Event Series|]/origin/value desc"
         }
 
-        headers = {'content-type': 'application/json', "Authorization": f"Basic {TOKEN}"}
-        r = requests.post(url=f"{API_ENDPOINT}/query/csv", data=json.dumps(data), headers=headers)
+        print(f"{API_ENDPOINT}/query/csv")
+
+        r = requests.post(url=f"{API_ENDPOINT}/query/csv", data=json.dumps(data), headers=self.headers)
 
         print(r.status_code)
-        print(r.json())
+        print(r)
 
         if r.ok:
             logger.info("successfully got csv")
-            return r.json()
+            return r
         raise Exception("something went wrong")
